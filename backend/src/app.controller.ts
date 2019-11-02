1dd58fe25b4dfe4bfd1b2e94e3246666b6fd86e5
felipeinfantino <felipe.infantino@hotmail.com>
2019-11-02 14:02:25 +0100

    working but messy

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -27,6 +27,48 @@ export class AppController {
     });
     const page = await browser.newPage();
     await page.goto('https://www.google.de/');
+    // step 1: (screenshot)
+    await page.evaluate(() => {
+      // step 2 : input field (screenshot)
+      const inputField = document.getElementsByClassName('gLFyf gsfi')[0] as HTMLInputElement;
+      inputField.value = 'test'; 
+      // step 3 : click the button
+      const submitButton = document.getElementsByClassName('gNO89b')[0] as HTMLInputElement;
+      submitButton.click();
+    });
+    // step 3.1 wait for click to response
+    await page.waitForNavigation();
+    const p =await page.evaluate(() => {
+      const allDivs = document.getElementsByClassName('r');
+      let t = '';
+      for(const div of allDivs){
+        t += ((div.childNodes[0] as HTMLAnchorElement).href + ';');
+      }
+      return t;
+    });
+    console.log(p);
+    const buffer = await page.screenshot({fullPage: true});
+    const base64 = buffer.toString('base64');
+    this.currentBase64 = base64;
+    await browser.close();
+    return base64;
+  }
+
+  @Get('test2')
+  async test2(){
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.google.de/');
+    await page.evaluate(() => {
+      const inputField = document.getElementsByClassName('gLFyf gsfi')[0] as HTMLInputElement;
+      inputField.value = 'test';
+      const submitButton = document.getElementsByClassName('gNO89b')[0] as HTMLInputElement;
+      submitButton.click();
+    });
+    await page.waitForNavigation();
     const buffer = await page.screenshot({fullPage: true});
     const base64 = buffer.toString('base64');
     this.currentBase64 = base64;
