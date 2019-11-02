7f49c8a3ddf3b93be8d31e40677d1315a1f4cdf7
felipeinfantino <felipe.infantino@hotmail.com>
2019-11-02 15:37:03 +0100

    puppeteer

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,16 +1,101 @@
 import { Controller, Get, Post, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { isIntersectionTypeAnnotation } from '@babel/types';
 const puppeteer = require('puppeteer');
 
 @Controller()
 export class AppController {
+
+  browser;
+  page;
   constructor(private readonly appService: AppService) {}
   currentValue = 'Hello word';
   currentBase64 = '';
+  currentLinks = [];
+
+  
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('initialize')
+  async init() {
+    this.browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    this.page = await this.browser.newPage();
+    return 'initialized';
+  }
+
+  @Get('openGoogle')
+  async openGoogle(){
+    await this.page.goto('https://www.google.de/');
+    const buffer = await this.page.screenshot({fullPage: true});
+    const base64 = buffer.toString('base64');
+    this.currentBase64 = base64;
+    return 'success';
+  }
+
+  @Get('inputOnGoolge/:userInput')
+  async setGoogleInput(@Param() userInputAsJson: string){
+    const userInput = userInputAsJson['userInput'];
+    await this.page.evaluate((userInput) => {
+      const inputField = document.getElementsByClassName('gLFyf gsfi')[0] as HTMLInputElement;
+      inputField.value = userInput;
+    }, userInput);
+    const buffer = await this.page.screenshot({fullPage: true});
+    const base64 = buffer.toString('base64');
+    this.currentBase64 = base64;
+    return 'input success';
+  }
+
+  @Get('clickGoogle')
+  async clickGoogle(){
+    await this.page.evaluate(() => {
+      const submitButton = document.getElementsByClassName('gNO89b')[0] as HTMLInputElement;
+      submitButton.click();
+    });
+    await this.page.waitForNavigation();
+    const buffer = await this.page.screenshot({fullPage: true});
+    const base64 = buffer.toString('base64');
+    const allLinksInString =await this.page.evaluate(() => {
+      const allDivs = document.getElementsByClassName('r');
+      let allLinks = '';
+      for(const div of allDivs){
+        allLinks += ((div.childNodes[0] as HTMLAnchorElement).href + ';');
+      }
+      return allLinks;
+    });
+    for(const link of allLinksInString.split(';')){
+      if(link) {
+        this.currentLinks.push(link);
+      }
+    }
+    this.currentBase64 = base64;
+    return 'click google';
+  }
+
+
+  @Get('goToLink/:number')
+  async goToLink(@Param('number') number: string){
+    let numberAsANumber = Number(number);
+    numberAsANumber -= 1;
+    console.log(this.currentLinks);
+    console.log(numberAsANumber);
+    console.log(this.currentLinks[numberAsANumber]);
+    await this.page.goto(this.currentLinks[numberAsANumber]);
+    const buffer = await this.page.screenshot({fullPage: true});
+    const base64 = buffer.toString('base64');
+    this.currentBase64 = base64;
+    return 'go to link succes';
+  }
+
+  @Get('currentBase64')
+  getCurrentBase64(){
+    return this.currentBase64;
   }
 
   @Get('backendData')
