5a9a8145af39d376af725f538e6e85d528a0cc16
felipeinfantino <felipe.infantino@hotmail.com>
2019-11-02 13:21:35 +0100

    new state

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Post, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+const puppeteer = require('puppeteer');
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   currentValue = 'Hello word';
-
+  currentBase64 = '';
 
   @Get()
   getHello(): string {
@@ -16,6 +17,21 @@ export class AppController {
   getBackendData(){
     console.log("returning ", this.currentValue);
     return this.currentValue;
+  }
+
+  @Get('test')
+  async test(){
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.google.de/');
+    const buffer = await page.screenshot({fullPage: true});
+    const base64 = buffer.toString('base64');
+    this.currentBase64 = base64;
+    await browser.close();
+    return base64;
   }
 
   // should be post, but for simplicity
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -4,16 +4,22 @@ export class Header extends Component {
 
     state = {
         data: '',
+        imageSrc: '',
     }
 
     componentDidMount() {
-        this.interval = setInterval(() => {
-            axios.get(`http://localhost:3001/backendData`)
+        // this.interval = setInterval(() => {
+        //     axios.get(`http://localhost:3001/backendData`)
+        //       .then(res => {
+        //         console.log(res);
+        //         this.setState({data: res.data.data})
+        //       })
+        // }, 1000);
+        axios.get(`http://localhost:3001/test`)
               .then(res => {
-                console.log(res);
-                this.setState({data: res.data.data})
+                  console.log("recieveing image", res.data)
+                this.setState({imageSrc: 'data:image/png;base64, ' + res.data})
               })
-        }, 1000);
       }
       componentWillUnmount() {
         clearInterval(this.interval);
@@ -24,6 +30,7 @@ export class Header extends Component {
             <div>
                 This is the frontend, recieving data from backend
                 Data recieved from backend : {this.state.data}
+                {this.state.imageSrc === '' ? '' : <img src={this.state.imageSrc}/>}
             </div>
         )
     }
